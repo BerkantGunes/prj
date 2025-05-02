643015d39240743b57be34c43f7329e5a2fc3981
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-02 20:42:06 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ornek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OkulEntities ef = new OkulEntities();
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Ogrenci yeniOgr = new Ogrenci();
+
+            yeniOgr.ogrenciNo = txtOgrenciNo.Text;
+            yeniOgr.adSoyad = txtAdSoyad.Text;
+            yeniOgr.Adres = txtAdres.Text;
+
+            ef.Ogrenci.Add(yeniOgr);
+            ef.SaveChanges();
+        }
+    }
+}
